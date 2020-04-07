bde7416ae2c5002b511248115ec16d040e47c694
nitunwa <nitun.2@gmail.com>
2020-04-07 15:01:55 -0700

    update js file and add function to display ticket and price

--- js/scripts.js
+++ js/scripts.js
@@ -8,42 +8,63 @@ function Ticket(name, age, time) {
  
  // }
  
- Ticket.prototype.getPrice = function(){
-    var ticketOriginalPrice = $12;
-    if(this.age === "18-60" && this.time === "5-11" && this.status === firstReleaseMovie){
-        this.price; 
-       }
-    }
+Ticket.prototype.getPrice = function(){
+   var price = 12;
+   if (this.age === "0-12" || this.age === "60+") {
+      price -= 5;
+   }
+   if (this.name === "Resistance" || this.name === "Onward") {
+      price += 3;
+   } 
+   if (this.time === "9am-5pm") {
+      price -= 3;
+   }
+   
+   return price;
+
+}
+
+   //  if (this.age === "0-60" && this.time === "5pm-11pm" || this.name === "My Spy" || this.name === "Onward") {
+   //      this.price; // original price
+   //    } else if (this.age === "60+" && this.time === "5pm-11pm" || this.name === "My Spy" || this.name === "Onward") {
+   //       this.price; // senior first release evening
+   //    } else if (this.age === "0-60" && this.time === "9am-5pm" || this.name === "My Spy" || this.name === "Onward") {
+   //       this.price; // first release morning
+   //    } else if (this.age === "60+" && this.time === "9am-5pm" || this.name === "My Spy" || this.name === "Onward") {
+   //       this.price; // senior first release morning
+   //    } else if (this.age === "0-60" && this.time === "5pm-11pm" || this.name === "Madtown" || this.name === "Frozen") {
+   //       this.price; // not release evening price
+   //    } else if (this.age === "60+" && this.time === "5pm-11pm" || this.name === "Madtown" || this.name === "Frozen") {
+   //       this.price; // senior not release evening price
+   //    } else if (this.age === "0-60" && this.time === "9am-5pm" || this.name === "Madtown" || this.name === "Frozen") {
+   //       this.price; // not release morning price
+   //    } else if (this.age === "60+" && this.time === "9am-5pm" || this.name === "Madtown" || this.name === "Frozen") {
+   //       this.price; // senior not release morning price
+   //    }
+   
  //    } else if ()
- // }
+ // 
  
- var mySpy = new Ticket("My Spy","18-60","5pm-11pm")
- var Onward = new Ticket("Onward", "18-60", "5pm-11pm")
- var Madtown = new Ticket("Madtown", "18-60", "5pm-11pm")
- var Frozen = new Ticket("Frozen", "18-60", "5pm-11pm")
+ var mySpy = new Ticket("My Spy","0-60","5pm-11pm")
+ var Onward = new Ticket("Onward", "0-60", "5pm-11pm")
+ var Madtown = new Ticket("Madtown", "0-60", "5pm-11pm")
+ var Frozen = new Ticket("Frozen", "0-60", "5pm-11pm")
  
- //function displayTicket(ticketToDisplay) {
- 
- //}
+ function displayTicket(ticket) {
+   var ticketTotal = "<h2 class='movieName'>Movie: " + ticket.name + "<br>Time: " + ticket.time +"</h2><hr><p class=ticketAge: " + ticket.age + "</p><hr><h3 class='price'>Price: $" + ticket.getPrice() + "</h3";
+   $("#result").html(ticketTotal);
+ }
  
  
  
  $(document).ready(function(){
     $("#tickets").submit(function(event){
        event.preventDefault();
-       var status = "";
-       var firstReleaseMovie = $("#first-release-movie").val();
-       var nonFirstReleaseMovie = $("#non-first-release-movie").val();
-       var inputAge = $("#age").val();
-       var inputTime = $("#time").val();
-       if(firstReleaseMovie === ''){
-          status = nonFirstReleaseMovie;
-       }else{
-          status = firstReleaseMovie;
-       }
-       var ticket = new Ticket(status, inputAge, inputTime);
-       ticket.getPrice();
-       console.log(ticket);
+       var name = $("select#name").val();
+       var age = $("select#age").val();
+       var time = $("select#time").val();
+       var newTicket = new Ticket(name, age, time);
+       newTicket.getPrice();
+       displayTicket(newTicket);
     });
- }); 
-       
+ });
